345b6106d5c7264dcc29ab077ca72df871c6f574
Katuhito Hara <katuhitohara@gmail.com>
2020-08-17 17:42:38 +0900

    allauth認証機能

--- katupro003/urls.py
+++ katupro003/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('k_app003.urls')),
+    path('accounts/', include('allauth.urls')),
+    
 
 ]
